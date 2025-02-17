87c5d3de79d737f068b5e3cdfe4695123a42b9e0
khaledian <khaledian@dmpaya.ir>
2025-02-17 21:36:37 +0330

    maintainer push

---
+++ src/app.module.ts
@@ -0,0 +1,36 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
+
+@Module({
+  imports: [LoggerModule.forRootAsync({
+    imports : [ConfigModule],
+    useFactory:(configservice:ConfigService) => {
+      const isProduction = configservice.getOrThrow('NODE_ENV') === 'production'
+      return {
+        pinoHttp : {
+          transport : isProduction ? undefined : {
+            target : 'pino-pretty',
+            options : {
+              singleLine : true
+            }
+          },
+          level : isProduction ? 'info' : 'debug'
+        }
+      }
+    },
+    inject : [ConfigService]
+  }
+  
+  ),
+    ConfigModule.forRoot({
+    envFilePath : './.env',
+    isGlobal : true
+  }),UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
